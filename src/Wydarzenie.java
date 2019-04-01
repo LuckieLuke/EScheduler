be2ec4bfe673b9e92c079605b50e490cd4d560cc
laskowsl <295803@pw.edu.pl>
2019-04-01 23:01:47 +0200

    nie wiem co tu dalej robic na razie

--- src/Wydarzenie.java
+++ src/Wydarzenie.java
@@ -1,3 +1,15 @@
+import java.util.Date;
+
 public class Wydarzenie {
+    private Date data;
+    private String nazwa;
+    private String opis;
+
+    public Wydarzenie(Date data, String nazwa, String opis) {
+        this.data = data;
+        this.nazwa = nazwa;
+        this.opis = opis;
+    }
+
 
 }
